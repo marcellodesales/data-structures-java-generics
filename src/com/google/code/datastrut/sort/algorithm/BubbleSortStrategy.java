e98fc87d6cc8173223e5e76c80e4b3162db0199d
Marcello de Sales <marcello.desales@gmail.com>
2011-05-02 01:57:56 +0000

    Adding the refereces to the icc0612 explanation link on youtube to the bubble sort implementation.

--- src/com/google/code/datastrut/sort/algorithm/BubbleSortStrategy.java
+++ src/com/google/code/datastrut/sort/algorithm/BubbleSortStrategy.java
@@ -6,7 +6,8 @@ import com.google.common.base.Preconditions;
 
 /**
  * Sorts the given arrayList with the given comparator.
- * The description of the algorithm can be seen at http://www.youtube.com/watch?v=P00xJgWzz2c
+ * The description of the algorithm can be seen at http://www.youtube.com/watch?v=tT4bJB0J4H4 or 
+ * http://www.youtube.com/watch?v=P00xJgWzz2c
  *
  * @author Marcello de Sales (marcello.desales@gmail.com)
  *
@@ -28,7 +29,8 @@ public final class BubbleSortStrategy extends AbstractSortStrategy {
 
     /**
      * Sorts the given arrayList with the given comparator.
-     * The description of the algorithm can be seen at http://www.youtube.com/watch?v=P00xJgWzz2c
+     * The description of the algorithm can be seen at http://www.youtube.com/watch?v=tT4bJB0J4H4
+     * or http://www.youtube.com/watch?v=P00xJgWzz2c
      * @param <Type> any type.
      * @param arrayList the array list of the given type.
      * @param comparator is the comparator that compares elements of the given type.
